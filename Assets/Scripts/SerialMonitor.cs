782a31b846ca73cc9e13cb0a4a7e3cd2a8800812
Andrew Huard <75150911+ahuard0@users.noreply.github.com>
2023-03-11 19:39:16 -0800

    Initial commit

---
+++ Assets/Scripts/SerialMonitor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Huard
+{
+    public class SerialMonitor : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        /*
+        *      Updates the Unity Inspector.
+        *      
+        *      Parameters
+        *      ----------
+        *      None
+        *      
+        *      Returns
+        *      -------
+        *      None
+        */
+        private void OnInspectorGUI()
+        {
+            
+        }
+    }
+}
